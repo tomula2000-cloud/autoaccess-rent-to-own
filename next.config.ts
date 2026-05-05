91b8af2d9bd88ca5565c10bf2060edb235c5d59d
Thomas Chimbwa <tomula2000@Thomass-MacBook-Air.local>
2026-05-05 20:38:53 +0300

    Remove audio CSP header blocker

--- next.config.ts
+++ next.config.ts
@@ -15,20 +15,7 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  async headers() {
-    return [
-      {
-        source: "/audio/:file*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'none'; media-src 'self' https://*.public.blob.vercel-storage.com; style-src 'unsafe-inline'; img-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
